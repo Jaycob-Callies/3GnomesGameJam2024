a354b07860340a7616bc9ce92ec455000f6cdda0
Commettey <162769055+Commettey@users.noreply.github.com>
2024-03-09 13:36:53 -0600

    Stuff i made
    i dont know what im doing

---
+++ Scripts/CameraStuff.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStuff : MonoBehaviour
+{
+    // i want the camera to follow the player with a clamp but i just havent wrote it yet
+}
