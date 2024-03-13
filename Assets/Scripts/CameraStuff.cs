42048586b45d53ad19bd2ade7a2e6bf55f81f0ad
Commettey <162769055+Commettey@users.noreply.github.com>
2024-03-13 14:02:18 -0500

    Fix

--- Assets/Scripts/CameraStuff.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraStuff : MonoBehaviour
-{
-    // i want the camera to follow the player with a clamp but i just havent wrote it yet
-}
